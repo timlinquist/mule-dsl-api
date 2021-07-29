23c1210aae53ea17f65d2f9bf63c16a268942b8d
Rodrigo Merino <elrodro83@users.noreply.github.com>
2021-07-29 09:57:35 -0300

    MULE-19670: Add the group name to the TypeDefinition (#92) (#93)

--- src/main/java/org/mule/runtime/dsl/api/component/TypeDefinitionVisitor.java
+++ src/main/java/org/mule/runtime/dsl/api/component/TypeDefinitionVisitor.java
@@ -29,8 +29,21 @@ public interface TypeDefinitionVisitor {
    * @param attributeName the name of the configuration attribute holding the type definition. Most likely a fully qualified java
    *                      class name.
    * @param enforcedClass the name of the class from which the one defined in the attribute must be an instance of.
+   * 
+   * @deprecated Use {@link #onConfigurationAttribute(String, String, Class)} instead.
    */
+  @Deprecated
   void onConfigurationAttribute(String attributeName, Class<?> enforcedClass);
+
+  /**
+   * Invoked when the {@link TypeDefinition} it's defined from a configuration attribute of the component
+   *
+   * @param attributeName the name of the parameter group containing the attribute.
+   * @param attributeName the name of the configuration attribute holding the type definition. Most likely a fully qualified java
+   *                      class name.
+   * @param enforcedClass the name of the class from which the one defined in the attribute must be an instance of.
+   */
+  void onConfigurationAttribute(String groupName, String attributeName, Class<?> enforcedClass);
 
   /**
    * Invoked when the {@link TypeDefinition} it's defined to be a map entry.
