d8a914dd5a77a54f76583d0d09074a10243e488d
Miguel Ortiz <mlompax@gmail.com>
2023-08-29 12:07:37 -0300

    W-13839953 Change License Headers (#160)

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,5 +1,8 @@
 /*
  * Copyright 2023 Salesforce, Inc. All rights reserved.
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
  */
 /**
  * Module with the API to define how to build runtime objects from configuration files.
