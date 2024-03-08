7a378fa474315b8070cd18334019bde786cbcf83
Rodrigo Merino <elrodro83@users.noreply.github.com>
2024-03-08 12:56:29 -0300

    W-14572236: Remove killswitches (#187)

--- src/main/java/org/mule/runtime/dsl/internal/xerces/xni/parser/DefaultXmlGrammarPoolManager.java
+++ src/main/java/org/mule/runtime/dsl/internal/xerces/xni/parser/DefaultXmlGrammarPoolManager.java
@@ -6,19 +6,15 @@
  */
 package org.mule.runtime.dsl.internal.xerces.xni.parser;
 
-import static java.lang.Boolean.parseBoolean;
-import static java.lang.System.getProperty;
 import static java.lang.Thread.currentThread;
-import static java.util.Optional.empty;
 import static java.util.Optional.of;
-import static org.mule.runtime.api.util.MuleSystemProperties.MULE_DISABLE_DEPLOYMENT_SCHEMA_CACHE;
 
 import org.mule.apache.xerces.xni.grammars.XMLGrammarPool;
 import org.mule.apache.xerces.xni.parser.XMLEntityResolver;
+import org.mule.runtime.api.util.LazyValue;
 import org.mule.runtime.dsl.api.xerces.xni.factories.XmlEntityResolverFactory;
 import org.mule.runtime.dsl.api.xerces.xni.factories.XmlGathererErrorHandlerFactory;
 import org.mule.runtime.dsl.api.xerces.xni.factories.XmlSchemaProviderFactory;
-import org.mule.runtime.api.util.LazyValue;
 
 import java.util.Optional;
 
@@ -28,8 +24,6 @@ import java.util.Optional;
  * @since 1.4.0
  */
 public class DefaultXmlGrammarPoolManager {
-
-  private static final boolean IS_CACHE_DISABLED = parseBoolean(getProperty(MULE_DISABLE_DEPLOYMENT_SCHEMA_CACHE, "false"));
 
   private static final LazyValue<Optional<XMLGrammarPool>> INSTANCE = new LazyValue<>(DefaultXmlGrammarPoolManager::initialize);
 
@@ -43,17 +37,13 @@ public class DefaultXmlGrammarPoolManager {
 
 
   private static Optional<XMLGrammarPool> initialize() {
-    if (IS_CACHE_DISABLED) {
-      return empty();
-    } else {
-      final Thread thread = currentThread();
-      final ClassLoader currentClassLoader = thread.getContextClassLoader();
-      try {
-        thread.setContextClassLoader(DefaultXmlGrammarPoolManager.class.getClassLoader());
-        return doInitialize();
-      } finally {
-        thread.setContextClassLoader(currentClassLoader);
-      }
+    final Thread thread = currentThread();
+    final ClassLoader currentClassLoader = thread.getContextClassLoader();
+    try {
+      thread.setContextClassLoader(DefaultXmlGrammarPoolManager.class.getClassLoader());
+      return doInitialize();
+    } finally {
+      thread.setContextClassLoader(currentClassLoader);
     }
   }
 
